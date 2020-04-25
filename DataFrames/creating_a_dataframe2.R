742c0708a1ed07fd991388d620bf813b0859bac1
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-04-25 12:30:59 -0600

    Create creating_a_dataframe2.R

---
+++ DataFrames/creating_a_dataframe2.R
@@ -0,0 +1,4 @@
+#The planets_df data frame should have 8 observations and 5 variables. It has been made available in the workspace, so you can directly use it.
+
+# Check the structure of planets_df
+str(planets_df)
